ccc6b8500d082b3d011be93f842993b18a890f46
Haimasree <haimasree.de@gmail.com>
2020-12-01 23:51:32 +0100

    adding shebang

--- test-containers/test-all-models.sh
+++ test-containers/test-all-models.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 envNames=(sharedpy3keras1.2 sharedpy3keras1.2 sharedpy3keras1.2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 sharedpy3keras2 mpra-dragonn extended_coda mmsplice mmsplice-mtsplice deepmel framepool kipoisplice deeptarget attentivechrome bpnet-oskn)
 modelNames=(CpGenie/A549_ENCSR000DDI Divergent421 DeepCpG_DNA/Hou2016_HepG2_dna Basenji Basset HAL DeepSEA/variantEffects Optimus_5Prime labranchor CleTimer/customBP SiSp FactorNet/FOXA2/onePeak_Unique35_DGF MaxEntScan/5prime pwm_HOCOMOCO/human/AHR DeepBind/Arabidopsis_thaliana/RBP/D00283.001_RNAcompete_At_0284 lsgkm-SVM/Chip/OpenChrom/Cmyc/K562 rbp_eclip/AARS MPRA-DragoNN/DeepFactorizedModel extended_coda MMSplice/deltaLogitPSI MMSplice/mtsplice DeepMEL Framepool KipoiSplice/4 deepTarget AttentiveChrome/E003 BPNet-OSKN)
 
