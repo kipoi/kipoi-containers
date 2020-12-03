b39d44be01756291eb5b744bde8075ec2f3ca2ae
Haimasree <haimasree.de@gmail.com>
2020-12-03 19:32:34 +0100

    Update build-and-test-images.sh

--- build-and-test-images.sh
+++ build-and-test-images.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 envNames=(kipoi-base-env sharedpy3keras1.2 sharedpy3keras2 mpra-dragonn extended_coda mmsplice mmsplice-mtsplice deepmel framepool kipoisplice deeptarget attentivechrome bpnet-oskn)
-docker build -f dockerfiles/Dockerfile.kipoi-base-env -t haimasree/kipoi-docker:kipoi-base-env .
+docker build -f dockerfiles/Dockerfile.kipoi-base-env -t haimasree/kipoi-docker:kipoi-base-env . || exit 1
 
 for env in ${envNames[@]}; do
-    docker build -f dockerfiles/Dockerfile.${env} -t haimasree/kipoi-docker:${env} .
-    pytest -s test-containers/test_containers_from_command_line.py --image=haimasree/kipoi-docker:${env}
-    docker system prune -a -f
+    docker build -f dockerfiles/Dockerfile.${env} -t haimasree/kipoi-docker:${env} . || exit 1
+    pytest -s test-containers/test_containers_from_command_line.py --image=haimasree/kipoi-docker:${env} || exit 1
+    docker system prune -a -f 
 done
 
 docker system prune -a -f
