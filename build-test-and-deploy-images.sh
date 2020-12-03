27b72748a43bfa119d3db16755845c34a7af3da3
Haimasree <haimasree.de@gmail.com>
2020-12-03 20:01:52 +0100

    Adding test safeties

--- build-test-and-deploy-images.sh
+++ build-test-and-deploy-images.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 envNames=(kipoi-base-env sharedpy3keras1.2 sharedpy3keras2 mpra-dragonn extended_coda mmsplice mmsplice-mtsplice deepmel framepool kipoisplice deeptarget attentivechrome bpnet-oskn)
-docker build -f dockerfiles/Dockerfile.kipoi-base-env -t haimasree/kipoi-docker:kipoi-base-env .
-docker push haimasree:kipoi-base-env
+docker build -f dockerfiles/Dockerfile.kipoi-base-env -t haimasree/kipoi-docker:kipoi-base-env . || exit 1
+docker push haimasree:kipoi-base-env || exit 1
 
 for env in ${envNames[@]}; do
-    docker build -f dockerfiles/Dockerfile.${env} -t haimasree/kipoi-docker:${env} .
-    pytest -s test-containers/test_containers_from_command_line.py --image=haimasree/kipoi-docker:${env}
-    docker push haimasree/kipoi-docker:${env}
+    docker build -f dockerfiles/Dockerfile.${env} -t haimasree/kipoi-docker:${env} . || exit 1
+    pytest -s test-containers/test_containers_from_command_line.py --image=haimasree/kipoi-docker:${env} || exit 1
+    docker push haimasree/kipoi-docker:${env} || exit 1
     docker system prune -a -f
 done
 
