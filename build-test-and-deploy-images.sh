88a4a04c8e9536037ef292e398368485004f906f
Haimasree <haimasree.de@gmail.com>
2020-12-10 13:20:57 +0100

    Adding seqvec

--- build-test-and-deploy-images.sh
+++ build-test-and-deploy-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-envNames=(kipoi-base-env sharedpy3keras1.2 sharedpy3keras2 mpra-dragonn extended_coda mmsplice mmsplice-mtsplice deepmel framepool kipoisplice deeptarget attentivechrome bpnet-oskn)
+envNames=(kipoi-base-env sharedpy3keras1.2 sharedpy3keras2 mpra-dragonn extended_coda mmsplice mmsplice-mtsplice deepmel framepool kipoisplice deeptarget attentivechrome bpnet-oskn seqvec)
 docker build -f dockerfiles/Dockerfile.kipoi-base-env -t haimasree/kipoi-docker:kipoi-base-env . || exit 1
 docker push haimasree:kipoi-base-env || exit 1
 
