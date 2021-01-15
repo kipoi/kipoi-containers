0f9871e148ee34146cfca3b1eb480b5f6f6842fd
Haimasree <haimasree.de@gmail.com>
2021-01-15 14:04:45 +0100

    Update update.py

--- model-updater/update.py
+++ model-updater/update.py
@@ -1,4 +1,14 @@
-import sys
+import subprocess
+
+
+def get_kipoi_model_head():
+    process = subprocess.Popen(
+        ["git", "ls-remote", "https://github.com/kipoi/models", "HEAD"],
+        stdout=subprocess.PIPE,
+    )
+    output = process.communicate()[0].decode("utf-8")
+    return output.split("\t")[0]
+
 
 if __name__ == "__main__":
-    print(f"Hello world {sys.argv[1]}")
+    print(get_kipoi_model_head())
