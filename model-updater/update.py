da356a25775b18d19d2518e520d1b2a1fec94915
Haimasree <haimasree.de@gmail.com>
2021-01-15 18:04:49 +0100

    Can figure out which model has been updated

--- model-updater/update.py
+++ model-updater/update.py
@@ -11,19 +11,24 @@ def get_kipoi_model_head():
     output = process.communicate()[0].decode("utf-8").split("\t")[0]
     return output
             
-def get_diff(source_commit_hash, target_commit_hash, access_token):
+def get_list_of_updated_models(source_commit_hash, target_commit_hash, access_token):
     g = Github(access_token)
     kipoi_model_repo = g.get_organization("kipoi").get_repo("models")
     comparison_obj = kipoi_model_repo.compare(base=source_commit_hash, head=target_commit_hash)
-    for f in comparison_obj.files:
-        print(f.filename)
+    return list(dict.fromkeys([f.filename.split('/')[0] for f in comparison_obj.files]))
+
+def update_or_add_model_container(model):
+    print(model)
+
 
 if __name__ == "__main__":
     with open('./model-updater/kipoi-model-repo-hash', 'r') as kipoimodelrepohash:
         source_commit_hash = kipoimodelrepohash.readline()
         target_commit_hash = get_kipoi_model_head()
-        if source_commit_hash != target_commit_hash:
-            get_diff(source_commit_hash=source_commit_hash, target_commit_hash=target_commit_hash, access_token=sys.argv[1])
-            print("The repo must be updated")
-        else:
-            print("No need to update the repo")
+    if source_commit_hash != target_commit_hash:
+        list_of_updated_models = get_list_of_updated_models(source_commit_hash=source_commit_hash, target_commit_hash=target_commit_hash, access_token=sys.argv[1])
+        for model in list_of_updated_models:
+            update_or_add_model_container(model=model)
+    else:
+        print("No need to update the repo")
+        
