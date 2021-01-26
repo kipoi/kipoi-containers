950c6415b371ce893d7b0ffeb11ad3bc5bc6b181
Haimasree <haimasree.de@gmail.com>
2021-01-26 14:42:13 +0100

    Reverting back updateoradd

--- modelupdater/updateoradd.py
+++ modelupdater/updateoradd.py
@@ -116,17 +116,13 @@ def add(model_group, kipoi_model_repo, kipoi_container_repo):
     print(f"Adding {model_group}")
 
     # Create a new dockerfile
-    write_docker_file = subprocess.Popen(
+    subprocess.call(
         [
             "sh",
             "./dockerfiles/dockerfile-generator.sh",
             f"{model_group}",
         ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
     )
-    (done, fail) = write_docker_file.communicate()
-    write_docker_file.wait()
 
     # Build a docker image
     dockerfile_path = f"./dockerfiles/Dockerfile.{model_group.lower()}"
