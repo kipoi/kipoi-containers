b2ae6ac51fa89fcf7c788a1f483104d556c94c5d
Haimasree <haimasree.de@gmail.com>
2021-01-26 18:25:59 +0100

    Update dockerfile-generator.sh

--- dockerfiles/dockerfile-generator.sh
+++ dockerfiles/dockerfile-generator.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 newmodelname=$1
 imagename="$(tr [A-Z] [a-z] <<< "$newmodelname")"
 sed "s/modelname/$newmodelname/g" ./dockerfiles/Dockerfile.template > ./dockerfiles/Dockerfile.${imagename}
