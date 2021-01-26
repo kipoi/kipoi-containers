660e26a5cd5c4c2532bb62044d32c3661010b812
Haimasree <haimasree.de@gmail.com>
2021-01-26 02:05:26 +0100

    Test setup ready

--- test-modelupdater/test_updateoradd.py
+++ test-modelupdater/test_updateoradd.py
@@ -5,37 +5,35 @@ import pytest
 from modelupdater.updateoradd import update, add
 
 
-@pytest.fixture
-def mock_get_list_of_models_from_repo():
-    def mock_ret(*args, **kwargs):
-        return ["CleTimer/customBP", "CleTimer/default"]
-
-
 class TestServerCode(object):
     model_group_to_update = ""
     image_to_update = ""
     model_group_to_add = ""
 
-    # def test_update(self):
-    #     assert self.model_group_to_update
-    #     assert self.image_to_update
-    #     client = docker.from_env()
-    #     original_shortid = client.images.get(self.image_to_update).short_id
-    #     update(
-    #         model=self.model_group_to_update,
-    #         name_of_docker_image=self.image_to_update,
-    #     )
-    #     assert (
-    #         client.images.get(self.image_to_update).short_id
-    #         != original_shortid
-    #     )
+    def test_update(self):
+        assert self.model_group_to_update
+        assert self.image_to_update
+        client = docker.from_env()
+        original_shortid = client.images.get(self.image_to_update).short_id
+        update(
+            model=self.model_group_to_update,
+            name_of_docker_image=self.image_to_update,
+        )
+        assert (
+            client.images.get(self.image_to_update).short_id
+            != original_shortid
+        )
 
     def test_add(self, monkeypatch):
-        assert self.model_group_to_add
+        def mockreturn(*args, **kwargs):
+            return ["CleTimer/customBP", "CleTimer/default"]
+
         monkeypatch.setattr(
             "modelupdater.updateoradd.get_list_of_models_from_repo",
-            mock_get_list_of_models_from_repo,
+            mockreturn,
         )
+
+        assert self.model_group_to_add
         add(
             model_group=self.model_group_to_add,
             kipoi_model_repo=None,
