76406e63806e635f64b8a2961a65f6024ae38400
Haimasree <haimasree.de@gmail.com>
2021-01-26 12:51:23 +0100

    Finally everything working

--- test-modelupdater/test_updateoradd.py
+++ test-modelupdater/test_updateoradd.py
@@ -14,19 +14,19 @@ class TestServerCode(object):
     image_to_update = ""
     model_group_to_add = ""
 
-    # def test_update(self):
-    #     assert self.model_group_to_update
-    #     assert self.image_to_update
-    #     client = docker.from_env()
-    #     original_shortid = client.images.get(self.image_to_update).short_id
-    #     update(
-    #         model=self.model_group_to_update,
-    #         name_of_docker_image=self.image_to_update,
-    #     )
-    #     assert (
-    #         client.images.get(self.image_to_update).short_id
-    #         != original_shortid
-    #     )
+    def test_update(self):
+        assert self.model_group_to_update
+        assert self.image_to_update
+        client = docker.from_env()
+        original_shortid = client.images.get(self.image_to_update).short_id
+        update(
+            model=self.model_group_to_update,
+            name_of_docker_image=self.image_to_update,
+        )
+        assert (
+            client.images.get(self.image_to_update).short_id
+            != original_shortid
+        )
 
     def test_add(self, monkeypatch):
         def mock_get_list_of_models_from_repo(*args, **kwargs):
