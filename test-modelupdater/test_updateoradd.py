dfecf5ae18bfceef297ea915cb8e6251a016035a
Haimasree <haimasree.de@gmail.com>
2021-01-26 02:54:28 +0100

    Adding full tests

--- test-modelupdater/test_updateoradd.py
+++ test-modelupdater/test_updateoradd.py
@@ -1,8 +1,12 @@
 import docker
+import json
 from pathlib import Path
 import pytest
+import shutil
 
 from modelupdater.updateoradd import update, add
+from ruamel.yaml import round_trip_load, round_trip_dump
+from ruamel.yaml.scalarstring import DoubleQuotedScalarString
 
 
 class TestServerCode(object):
@@ -25,17 +29,127 @@ class TestServerCode(object):
         )
 
     def test_add(self, monkeypatch):
-        def mockreturn(*args, **kwargs):
+        def mock_get_list_of_models_from_repo(*args, **kwargs):
             return ["CleTimer/customBP", "CleTimer/default"]
 
         monkeypatch.setattr(
             "modelupdater.updateoradd.get_list_of_models_from_repo",
-            mockreturn,
+            mock_get_list_of_models_from_repo,
+        )
+
+        with open(
+            Path(__file__).resolve()
+            / "../test-containers"
+            / "image-name-to-model.json",
+            "r",
+        ) as infile:
+            image_name_to_model_dict = json.load(infile)
+
+        with open(
+            Path(__file__).resolve()
+            / "../test-containers"
+            / "model-group-to-image-name.json",
+            "r",
+        ) as infile:
+            model_group_to_image_dict = json.load(infile)
+
+        workflow_test_images_file_path = (
+            Path(__file__).resolve() / "../.github/workflows/test-images.yml"
+        )
+        workflow_build_and_test_images_file_path = (
+            Path(__file__).resolve()
+            / "../.github/workflows/build-and-test-images.yml"
+        )
+
+        shutil.copy(
+            workflow_test_images_file_path,
+            Path(__file__).resolve() / "tmp-test-images.yml",
+        )
+        shutil.copy(
+            workflow_build_and_test_images_file_path,
+            Path(__file__).resolve() / "tmp-build-and-test-images.yml",
         )
 
         assert self.model_group_to_add
+
         add(
             model_group=self.model_group_to_add,
             kipoi_model_repo=None,
             kipoi_container_repo=None,
+        )
+
+        # Revert the change
+        dockerfile_path = (
+            Path(__file__).resolve()
+            / f"../dockerfiles/Dockerfile.{self.model_group_to_add.lower()}"
+        )
+        assert dockerfile_path.exists()
+        dockerfile_path.unlink()
+
+        with open(
+            Path(__file__).resolve()
+            / "../test-containers"
+            / "image-name-to-model.json",
+            "r",
+        ) as infile:
+            new_image_name_to_model_dict = json.load(infile)
+        assert (
+            "haimasree/kipoi-docker:cletimer" in new_image_name_to_model_dict
+        )
+        assert new_image_name_to_model_dict[
+            "haimasree/kipoi-docker:cletimer"
+        ] == ["CleTimer/customBP", "CleTimer/default"]
+
+        with open(
+            Path(__file__).resolve()
+            / "../test-containers"
+            / "model-group-to-image-name.json",
+            "r",
+        ) as infile:
+            new_model_group_to_image_dict = json.load(infile)
+        assert "CleTimer" in new_model_group_to_image_dict
+        assert (
+            new_model_group_to_image_dict["CleTimer"]
+            == "haimasree/kipoi-docker:cletimer"
+        )
+
+        with open(
+            Path(__file__).resolve()
+            / "../test-containers"
+            / "model-group-to-image-name.json",
+            "w",
+        ) as fp:
+            json.dump(model_group_to_image_dict, fp, indent=2)
+
+        with open(
+            Path(__file__).resolve()
+            / "../test-containers"
+            / "image-name-to-model.json",
+            "w",
+        ) as fp:
+            json.dump(image_name_to_model_dict, fp, indent=2)
+
+        with open(workflow_build_and_test_images_file_path, "r") as f:
+            data = round_trip_load(f, preserve_quotes=True)
+            assert (
+                DoubleQuotedScalarString(self.model_group_to_add)
+                in data["jobs"]["buildandtest"]["strategy"]["matrix"]["image"]
+            )
+
+        with open(
+            workflow_test_images_file_path,
+            "r",
+        ) as f:
+            data = round_trip_load(f, preserve_quotes=True)
+            assert (
+                DoubleQuotedScalarString(self.model_group_to_add)
+                in data["jobs"]["test"]["strategy"]["matrix"]["model"]
+            )
+        shutil.copy(
+            Path(__file__).resolve() / "tmp-test-images.yml",
+            workflow_test_images_file_path,
+        )
+        shutil.copy(
+            Path(__file__).resolve() / "tmp-build-and-test-images.yml",
+            workflow_build_and_test_images_file_path,
         )
