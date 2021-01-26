f19266eb95005dc56826cd47c6d48560f4a6b533
Haimasree <haimasree.de@gmail.com>
2021-01-26 19:12:38 +0100

    Bug fix attempt

--- dockerfiles/dockerfile-generator.sh
+++ dockerfiles/dockerfile-generator.sh
@@ -2,4 +2,5 @@
 
 newmodelname=$1
 imagename="$(tr [A-Z] [a-z] <<< "$newmodelname")"
-sed "s/modelname/$newmodelname/g" ./dockerfiles/Dockerfile.template > ./dockerfiles/Dockerfile.${imagename}
+dir=$(pwd)
+sed "s/modelname/$newmodelname/g" ${dir}/dockerfiles/Dockerfile.template > ${dir}/dockerfiles/Dockerfile.${imagename}
