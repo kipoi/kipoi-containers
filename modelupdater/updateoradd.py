bde609822a719124abb070b1a77f7697dbdb786b
Haimasree <haimasree.de@gmail.com>
2021-01-31 23:31:45 +0100

    doc added

--- modelupdater/updateoradd.py
+++ modelupdater/updateoradd.py
@@ -9,6 +9,18 @@ from .updater import ModelUpdater
 
 class ModelSyncer:
     def __init__(self, github_obj):
+        """
+        This function initializes several class variables with the
+        logged in github instance an from kipoi-model-repo-hash
+        and model-group-to-image-name.json.
+
+        Parameters
+        ----------
+        github_obj : Github instance
+            Logged in github instance that is used to get the models
+            repo, container repo and current commit hash of kipoi model
+            repo.
+        """
         self.github_obj = github_obj
         self.kipoi_container_repo = self.github_obj.get_user().get_repo(
             "kipoi-containers"
@@ -31,6 +43,11 @@ class ModelSyncer:
             self.model_group_to_image_dict = json.load(infile)
 
     def get_list_of_updated_models(self):
+        """
+        Figures out which model groups have been updated or added between
+        the commit hash stored in ./kipoi-model-repo-hash and current
+        commit hash of kipoi model repo.
+        """
         comparison_obj = self.kipoi_model_repo.compare(
             base=self.source_commit_hash, head=self.target_commit_hash
         )
@@ -44,6 +61,15 @@ class ModelSyncer:
         )
 
     def update_or_add_model_container(self, model):
+        """
+        Calls appropariate functions based on whether a model group has
+        been updated or added.
+
+        Parameters
+        ----------
+        model : str
+            Model group to update or add
+        """
         if model in self.model_group_to_image_dict:
             model_updater = ModelUpdater()
             model_updater.update(
@@ -59,6 +85,10 @@ class ModelSyncer:
             model_adder.add()
 
     def sync(self):
+        """
+        Sync this repository with https://github.com/kipoi/models and update the
+        commithash in kipoi-model-repo-hash if everything is fine
+        """
         if self.source_commit_hash != self.target_commit_hash:
             self.get_list_of_updated_models()
             for model in self.list_of_updated_models:
@@ -74,5 +104,9 @@ class ModelSyncer:
 
 
 if __name__ == "__main__":
+    """
+    Main function which logs in to github with a PAT that is stored in
+    an environmental variable called "GITHUB_PAT"
+    """
     model_syncer = ModelSyncer(github_obj=Github(os.environ["GITHUB_PAT"]))
     model_syncer.sync()
