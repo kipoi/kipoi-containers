61c024b0246c170af81131f659a8318f5e15540d
Haimasree <haimasree.de@gmail.com>
2021-03-28 20:02:31 +0200

    Trying something new out

--- test-modelupdater/test_updateoradd.py
+++ test-modelupdater/test_updateoradd.py
@@ -21,7 +21,7 @@ def test_update(model_group_to_update, image_to_update, monkeypatch):
         return True
 
     monkeypatch.setattr(
-        "modelupdater.helper.push_docker_image",
+        "modelupdater.updater.helper.push_docker_image",
         staticmethod(mock_push_docker_image),
     )
 
@@ -55,7 +55,7 @@ def test_add(monkeypatch):
         staticmethod(mock_is_compatible_with_existing_image),
     )
     monkeypatch.setattr(
-        "modelupdater.helper.push_docker_image",
+        "modelupdater.adder.helper.push_docker_image",
         staticmethod(mock_push_docker_image),
     )
 
