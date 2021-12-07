a0e68825c871116b10d98856497ab879b066778b
Haimasree <haimasree.de@gmail.com>
2021-12-07 14:05:26 +0100

    adding a proper test

--- test-containers/test_update_all_singularity_images.py
+++ test-containers/test_update_all_singularity_images.py
@@ -1,4 +1,5 @@
 from click.testing import CliRunner
+import json
 import pytest
 
 from kipoi_containers import update_all_singularity_images
@@ -10,7 +11,21 @@ def runner():
     yield runner
 
 
-def test_cli_correct_use(runner, monkeypatch):
+@pytest.fixture
+def demo_dict():
+    return
+    {
+        "url": "https://dummy.url",
+        "name": "wrong_name",
+        "md5": "78758738",
+    }
+
+
+def test_cli_correct_use(runner, monkeypatch, tmp_path, demo_dict):
+    demo_dir = tmp_path / "demo"
+    demo_dir.mkdir()
+    scrap_singularity_json = demo_dir / "scrap.json"
+
     def mock_build(*args, **kwargs):
         return ""
 
@@ -28,9 +43,6 @@ def test_cli_correct_use(runner, monkeypatch):
         return False
 
     def mock_cleanup(*args, **kwargs):
-        return
-
-    def mock_write_json(*args, **kwargs):
         return
 
     monkeypatch.setattr(
@@ -54,12 +66,44 @@ def test_cli_correct_use(runner, monkeypatch):
         mock_cleanup,
     )
     monkeypatch.setattr(
-        "kipoi_containers.update_all_singularity_images.write_json",
-        mock_write_json,
+        "kipoi_containers.update_all_singularity_images.MODEL_GROUP_TO_SINGULARITY_JSON",
+        scrap_singularity_json,
     )
-
+    singularity_content = {
+        "MMSplice/modularPredictions": {
+            "url": "https://zenodo.org/record/5643976/files/kipoi-docker_mmsplice.sif?download=1",
+            "name": "kipoi-docker_mmsplice",
+            "md5": "29438b52fafdde5f48658fdcd7a61c6c",
+        },
+        "MMSplice/mtsplice": {
+            "url": "https://zenodo.org/record/5643967/files/kipoi-docker_mmsplice-mtsplice.sif?download=1",
+            "name": "kipoi-docker_mmsplice-mtsplice",
+            "md5": "1b8dab773ad7b8d2299fb294b0e3216e",
+        },
+        "DeepMEL": {
+            "url": "https://zenodo.org/record/5643863/files/kipoi-docker_deepmel.sif?download=1",
+            "name": "kipoi-docker_deepmel",
+            "md5": "c1f7204b834bba9728c67e561952f8e8",
+        },
+    }
+    with open(scrap_singularity_json, "w") as file_handle:
+        json.dump(singularity_content, file_handle)
     result = runner.invoke(
         update_all_singularity_images.run_update,
         ["kipoi/kipoi-docker:mmsplice-mtsplice"],
     )
     assert result.exit_code == 0
+    with open(scrap_singularity_json, "r") as file_handle:
+        result_content = json.load(file_handle)
+
+    assert result_content.keys() == singularity_content.keys()
+    assert result_content["DeepMEL"] == singularity_content["DeepMEL"]
+    assert (
+        result_content["MMSplice/modularPredictions"]
+        == singularity_content["MMSplice/modularPredictions"]
+    )
+    assert result_content["MMSplice/mtsplice"] == {
+        "url": "https://dummy.url",
+        "name": "wrong_name",
+        "md5": "78758738",
+    }
