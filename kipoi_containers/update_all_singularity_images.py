a29e4650ee3f0186edbe7085a9be78c075a5c921
Haimasree <haimasree.de@gmail.com>
2022-01-04 17:04:38 +0100

    attempting to fix a bug in release workflow

--- kipoi_containers/update_all_singularity_images.py
+++ kipoi_containers/update_all_singularity_images.py
@@ -7,6 +7,7 @@ from kipoi_containers import singularityhandler
 from kipoi_containers.updateoradd import (
     MODEL_GROUP_TO_SINGULARITY_JSON,
     DOCKER_TO_MODEL_JSON,
+    MODEL_GROUP_TO_DOCKER_JSON,
 )
 from kipoi_containers.helper import populate_json, write_json
 
@@ -29,30 +30,37 @@ def run_update(docker_image: str) -> None:
     model_group_to_singularity_dict = populate_json(
         MODEL_GROUP_TO_SINGULARITY_JSON
     )
+    model_group_to_docker = populate_json(MODEL_GROUP_TO_DOCKER_JSON)
+    docker_to_model_group_dict_ci = {}
+    for model_group, docker_image in model_group_to_docker.json():
+        if docker_image in docker_to_model_group_dict_ci:
+            docker_to_model_group_dict_ci[docker_image].append(model_group)
+        else:
+            docker_to_model_group_dict_ci[docker_image] = [model_group]
     model_group_to_singularity_dict_ci = {
         k.lower(): v for k, v in model_group_to_singularity_dict.items()
     }
-    model_group = docker_image.split(":")[-1]
-    model_or_model_group = model_group.replace("-", "/")
+    model_or_model_group_list = docker_to_model_group_dict_ci[docker_image]
     singularity_pull_folder = os.environ.get(
         "SINGULARITY_PULL_FOLDER", Path(__file__).parent.resolve()
     )
-    singularity_handler = singularityhandler.SingularityHandler(
-        model_group=model_or_model_group,
-        docker_image_name=docker_image,
-        singularity_image_folder=singularity_pull_folder,
-        model_group_to_singularity_dict=model_group_to_singularity_dict_ci,
-    )
     docker_to_model_dict = populate_json(DOCKER_TO_MODEL_JSON)
-    models_to_test = docker_to_model_dict[docker_image]
-    singularity_handler.update(models_to_test)
-    model_group_to_singularity_dict = {
-        k: model_group_to_singularity_dict_ci[k.lower()]
-        for k in model_group_to_singularity_dict.keys()
-    }
-    write_json(
-        model_group_to_singularity_dict, MODEL_GROUP_TO_SINGULARITY_JSON
-    )
+    for model_or_model_group in model_or_model_group_list:
+        singularity_handler = singularityhandler.SingularityHandler(
+            model_group=model_or_model_group,
+            docker_image_name=docker_image,
+            singularity_image_folder=singularity_pull_folder,
+            model_group_to_singularity_dict=model_group_to_singularity_dict_ci,
+        )
+        models_to_test = docker_to_model_dict[docker_image]
+        singularity_handler.update(models_to_test)
+        model_group_to_singularity_dict = {
+            k: model_group_to_singularity_dict_ci[k.lower()]
+            for k in model_group_to_singularity_dict.keys()
+        }
+        write_json(
+            model_group_to_singularity_dict, MODEL_GROUP_TO_SINGULARITY_JSON
+        )
 
 
 if __name__ == "__main__":
