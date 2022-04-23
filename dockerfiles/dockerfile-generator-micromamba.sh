eb7eea3c1ade9e1ff12f58eff62ecdeb1f47933f
Haimasree <haimasree.de@gmail.com>
2022-04-23 17:39:27 +0200

    reverting micromamba

--- dockerfiles/dockerfile-generator-micromamba.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-newmodelname=$1
-imagename="$(tr [A-Z] [a-z] <<< "$newmodelname")"
-dir=$(pwd)
-sed "s/modelname/$newmodelname/g" ${dir}/dockerfiles/Dockerfile-micromamba.template > ${dir}/dockerfiles/Dockerfile.${imagename}-micromamba
