076f2e4961594c0903db880c8f7368d3da1bab7d
Haimasree <haimasree.de@gmail.com>
2022-05-27 12:48:38 +0200

    adding a test for building and testing images

---
+++ test-singularity/test_singularity_buildandexec.py
@@ -0,0 +1,20 @@
+from kipoi_containers.singularityhelper import (
+    build_singularity_image,
+    test_singularity_image,
+)
+
+
+def buildandtest_singularity_image(tmp_path):
+    singularity_dir = tmp_path / "singularity"
+    singularity_dir.mkdir()
+    singularity_image = "kipoi-docker_aparent-veff-slim.sif"
+    build_singularity_image(
+        name_of_docker_image="kipoi/kipoi-docker:aparent-veff-slim",
+        singularity_image_name=singularity_image,
+        singularity_image_folder=singularity_dir,
+    )
+    test_singularity_image(
+        singularity_image_folder=singularity_dir,
+        singularity_image_name=singularity_image,
+        model="APARENT/veff",
+    )
